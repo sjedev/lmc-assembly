576c08ec87d3333c5d17bae1e034535b22648950
S Jensen <111765576+sjedev@users.noreply.github.com>
2022-10-10 20:56:34 +0100

    Adds two numbers using variables

---
+++ 1. Basics/1.4.1 Variables.s
@@ -0,0 +1,19 @@
+00 INP
+01 STA 07
+02 INP
+03 STA 08
+04 ADD 07
+05 OUT
+06 HLT
+07 DAT 00
+08 DAT 00
+
+        inp
+        sta first
+        inp
+        sta second
+        add first
+        out
+        hlt
+first   dat
+second  dat
