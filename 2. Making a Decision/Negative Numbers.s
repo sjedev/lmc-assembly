c8eb0d34363bd41c5747b1fe0f2e5a52a094e645
S Jensen <111765576+sjedev@users.noreply.github.com>
2022-10-11 15:28:06 +0100

    Outputs P if input positive, N if negative

---
+++ 2. Making a Decision/Negative Numbers.s
@@ -0,0 +1,21 @@
+00 INP
+01 BRP 05
+02 LDA 08
+03 OTC
+04 HLT
+05 LDA 09
+06 OTC
+07 HLT
+08 DAT 78
+09 DAT 80
+
+         inp
+         brp positive
+         lda n
+         otc
+         hlt
+positive lda p
+         otc
+         hlt
+n        dat 78
+p        dat 80
